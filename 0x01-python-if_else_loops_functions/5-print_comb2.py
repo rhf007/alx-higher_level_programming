27e6cee553d40d17b8251144a8d713936ad618c6
rhf007 <rahofahazem512@gmail.com>
2023-08-08 14:53:46 -0700

    Py02

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(f"{i}")
-    print(f"{i:02}", end=", ")
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
