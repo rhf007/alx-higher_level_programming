55d338edc97c18fc7b396afdc634508f86e2274e
rhf007 <rahofahazem512@gmail.com>
2023-08-15 10:11:06 -0700

    Logic Seems Incomprehensible

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        return None
+    maxnum = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > maxnum:
+            maxnum = my_list[i]
+    return maxnum
