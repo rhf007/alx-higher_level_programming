389d7ccb412ff984fa65a4b1ae4f9e2e09d27f88
rhf007 <rahofahazem512@gmail.com>
2023-09-06 18:31:47 -0700

    2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,25 +15,24 @@ def matrix_divided(matrix, div):
         TypeError: If div is not int or float.
         ZeroDivisionError: If div is zero.
     """
-    if not isinstance(div, (int, float)):
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-    if not isinstance(matrix, list) or len(matrix) == 0:
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(matrix[0], list) or len(matrix[0]) <= 0:
         raise TypeError("matrix must be a matrix (list of lists) " +
                         "of integers/floats")
     for row in matrix:
-        if not isinstance(row, list) or len(row) == 0:
+        if not isinstance(row, list) or row == []:
             raise TypeError("matrix must be a matrix (list of lists) " +
                             "of integers/floats")
         if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError('Each row of the matrix must have the same size')
         for num in row:
-            if not isinstance(num, (int, float)):
+            if not isinstance(num, int) and not isinstance(num, float):
                 raise TypeError("matrix must be a matrix (list of lists) " +
                                 "of integers/floats")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(num / div, 2) for num in row] for row in matrix]
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/2-matrix_divided.txt")
+    return ([list(map(lambda num: round(num / div, 2), row)) for row in matrix])
