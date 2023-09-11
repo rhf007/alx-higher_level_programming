c962abebbe3f684a35cfcf33d2b5ae92063a95dd
rhf007 <rahofahazem512@gmail.com>
2023-09-11 11:39:05 -0700

    isinstance isn't the same as type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,16 @@
 
 
 def is_same_class(obj, a_class):
+    """
+    Function to check if obj is an instance a specified class
+
+    Args:
+        obj: object to check
+        a_class: class to check if object is from
+
+    Returns:
+        True if it is, flase otherwise
+    """
     if type(obj) is a_class:
         return True
     return False
