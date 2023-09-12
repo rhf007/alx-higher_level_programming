bf579ccb72181d56f4d7e0770ca0e3ffebdb7a4f
rhf007 <rahofahazem512@gmail.com>
2023-09-11 19:57:30 -0700

    ya

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""square module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+
+    def __init__(self, size):
+        """square function"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """area function"""
+
+        return self.__size ** 2
