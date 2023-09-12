d3ae50223418bd6754aa24787d6f9910b5743337
rhf007 <rahofahazem512@gmail.com>
2023-09-11 19:33:10 -0700

    nice

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""geometry module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+
+    def __init__(self, width, height):
+        """init method"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
