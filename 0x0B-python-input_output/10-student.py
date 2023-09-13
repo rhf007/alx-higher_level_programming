aaab28d2be9b5aed0decfb7d84c134e6c5c51f59
rhf007 <rahofahazem512@gmail.com>
2023-09-12 19:59:25 -0700

    sdcs

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,7 +4,9 @@
 
 class Student:
     """student class"""
+
     def __init__(self, first_name, last_name, age):
+        """initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
