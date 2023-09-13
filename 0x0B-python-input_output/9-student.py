7fa95bcd7e78811de618f18ee29429f9bf4eb9ce
rhf007 <rahofahazem512@gmail.com>
2023-09-12 19:50:28 -0700

    dscd

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""student to json module"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retreive json"""
+        return self.__dict__
