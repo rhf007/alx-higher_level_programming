2741ffdd7a892229d302f4c67a6e2584b4c2309e
rhf007 <rahofahazem512@gmail.com>
2023-09-12 19:54:36 -0700

    sdcs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""student to json module"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retreive json"""
+        if attrs is None:
+            return self.__dict__
+	dic = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dic[key] = value
+        return dic
