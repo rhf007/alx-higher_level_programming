f8ea24193876cdd1b18736362f16428501d20bce
rhf007 <rahofahazem512@gmail.com>
2023-09-15 07:31:03 -0700

    Baseball Bat

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
