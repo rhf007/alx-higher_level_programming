bb31e7499b680a04a39a564c14b2ae3c047699ef
rhf007 <rahofahazem512@gmail.com>
2023-09-15 08:12:11 -0700

    DOGMA

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,28 @@ class Square(Rectangle):
         """size setter"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """update square"""
+        if args is not None and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                    self.height = args[i]
+                elif i == 2:
+                    self.x = args[i]
+                else:
+                    self.y = args[i]
+        elif kwargs is not None and len(kwargs) != 0:
+            for kwarg in kwargs:
+                if kwarg == "id":
+                    self.id = kwargs[kwarg]
+                elif kwarg == "size":
+                    self.width = kwargs[kwarg]
+                    self.height = kwargs[kwarg]
+                elif kwarg == "x":
+                    self.x = kwargs[kwarg]
+                else:
+                    self.y = kwargs[kwarg]
