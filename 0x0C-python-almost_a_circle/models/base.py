052a7b824213b13cf80c54a4fa04ece108d37c7f
rhf007 <rahofahazem512@gmail.com>
2023-09-15 09:36:50 -0700

    The Heart From Your Hate

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base module"""
+import json
 
 
 class Base:
@@ -13,3 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON representation"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return (json.dumps(list_dictionaries))
