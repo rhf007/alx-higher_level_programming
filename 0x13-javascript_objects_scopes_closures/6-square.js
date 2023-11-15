3b21d0e6ad0588e237832c169fe577ab9e3b96f8
rhf007 <rahofahazem512@gmail.com>
2023-11-15 10:52:48 -0800

    extremely witty

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const ogsquare = require('./5-square');
+
+class Square extends ogsquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let r = '';
+      for (let j = 0; j < this.width; j++) {
+        r += c;
+      }
+      console.log(r);
+    }
+  }
+}
+
+module.exports = Square;
