4bcef2ccb28c3c53cc702f0ca17eb8a6c2774e6a
rhf007 <rahofahazem512@gmail.com>
2023-11-15 05:04:02 -0800

    ok seems like sort works with a little twist

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@
 const process = require('process');
 const args = process.argv;
 let argv = args.slice(2).map(Number);
-argv = argv.sort().reverse();
-if (argv.length <= 2) {
+argv = argv.sort((a, b) => b - a);
+if (argv.length < 2) {
   console.log(0);
 } else {
   console.log(parseInt(argv[1]));
