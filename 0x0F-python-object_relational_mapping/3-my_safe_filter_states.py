97ef799a87caa4155ab1e272a1e7459dff3e1c7a
rhf007 <rahofahazem512@gmail.com>
2023-11-16 12:55:49 -0800

    seems like execute's 2nd parameter is a lil unique

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module: Filter states by user input but safer cuz we're cool"""
+
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], port=3306,
+                     db=argv[3])
+cur = db.cursor()
+state = argv[4]
+cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (argv[4], ))
+rows = cur.fetchall()
+for row in rows:
+    print(row)
