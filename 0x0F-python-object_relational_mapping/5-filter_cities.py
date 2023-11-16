13c0f8ae6bedfd6b42b97dcfe3d8433a72971ce2
rhf007 <rahofahazem512@gmail.com>
2023-11-16 14:02:35 -0800

    tuples and their stuff ig

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""All cities by state module"""
+
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                     db=argv[3], port=3306)
+cur = db.cursor()
+cur.execute('SELECT cities.name FROM cities JOIN states ON'
+            ' cities.state_id = states.id WHERE states.name = %s;', (argv[4],))
+rows = cur.fetchall()
+cities = []
+for row in rows:
+    cities.append(row[0])
+print(", ".join(cities))
