833360108cad04ae15cdac973d9579c805632bbd
rhf007 <rahofahazem512@gmail.com>
2023-11-16 15:21:33 -0800

    sometimes python is unbelievable

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Get a state"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+engine = create_engine(uri)
+Session = sessionmaker()
+session = Session(bind=engine)
+n = argv[4]
+state = session.query(State).order_by(State.id).filter(State.name == n).first()
+if state is None:
+    print("Not found")
+else:
+    print("{}".format(state.id))
