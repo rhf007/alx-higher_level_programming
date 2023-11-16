8c89e92038549e8297e39db344d808687aa7c0a0
rhf007 <rahofahazem512@gmail.com>
2023-11-16 14:44:41 -0800

    gotta take a lot of practice

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""All states via SQLAlchemy"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                  argv[2], argv[3])
+engine = create_engine(uri)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+for row in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(row.id, row.name))
