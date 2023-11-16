964eb68a5473ab554c1c6ddf95b10d1b4b82fa3b
rhf007 <rahofahazem512@gmail.com>
2023-11-16 15:11:38 -0800

    gotta have biiiiig reference

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Contains a"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+engine = create_engine(uri)
+Session = sessionmaker()
+session = Session(bind=engine)
+for s in session.query(State).order_by(State.id).\
+                 filter(State.name.like('%a%')):
+    print("{}: {}".format(s.id, s.name))
