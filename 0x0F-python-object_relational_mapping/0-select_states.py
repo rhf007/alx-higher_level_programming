dedae8c89cafb2c1fdddb323bdfe13a9f8762066
rhf007 <rahofahazem512@gmail.com>
2023-11-17 06:26:42 -0800

    lets see whats wrong

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,13 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], port=3306,
-                     db=argv[3])
-cur = db.cursor()
-stmt = "SELECT * FROM states"
-cur.execute(stmt)
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+if __name__ == '__main__':
+    """lets try using this way to see if something is fixed"""
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], port=3306,
+                         db=argv[3])
+    cur = db.cursor()
+    stmt = "SELECT * FROM states"
+    cur.execute(stmt)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
