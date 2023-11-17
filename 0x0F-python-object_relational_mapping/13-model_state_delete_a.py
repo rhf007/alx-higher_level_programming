cf1f8c1c7cb87537fb463d5d808173637c1be137
rhf007 <rahofahazem512@gmail.com>
2023-11-17 05:39:37 -0800

    muscle memory, huh?

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Delete states"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+engine = create_engine(uri)
+Session = sessionmaker()
+session = Session(bind=engine)
+for s in session.query(State).filter(State.name.like('%a%')):
+    session.delete(s)
+session.commit()
