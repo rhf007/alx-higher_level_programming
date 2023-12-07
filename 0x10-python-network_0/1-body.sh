79f946039e8f049ed519a8ff195991290e9b0d34
rhf007 <rahofahazem512@gmail.com>
2023-12-07 00:11:28 -0800

    nice

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response, it does seem that -L doesnt get along with other options
+curl -X GET $1 -L
