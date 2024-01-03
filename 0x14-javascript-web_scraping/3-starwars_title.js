d482aa3efa0439364230af4e307732709d862a1b
rhf007 <rahofahazem512@gmail.com>
2024-01-03 08:54:22 -0800

    semistandard doesn't like bracket notations

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
-const
+const r = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+r.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
