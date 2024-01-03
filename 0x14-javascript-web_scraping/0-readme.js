371da283fb8618aab64449a4206c5abcc2b7da9c
rhf007 <rahofahazem512@gmail.com>
2024-01-03 07:49:13 -0800

    Okay my bad

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const file = require('fs');
-const argv = require('process.argv');
+const path = process.argv[2];
 
-const path = argv[2];
 file.readFile(path, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
